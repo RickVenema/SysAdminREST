6418a2b600ea5ec29cb1ebb247e174b1a7dd0d94
Rick Venema <rvenema@kza.nl>
2021-04-27 16:59:16 +0200

    Added 2 functions to be implemented

--- API/Docker.py
+++ API/Docker.py
@@ -8,3 +8,11 @@ class Docker:
     def get_docker_images():
         output = [f"{item}" for item in client.images.list()]
         return output
+
+    @staticmethod
+    def pull_docker_image():
+        pass
+
+    @staticmethod
+    def run_docker_image():
+        pass
